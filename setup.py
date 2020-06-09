fcffc271e0e465faecbb17370e22efe74bcb1d35
Kotaro Kikuchi <kotaro@pcl.cs.waseda.ac.jp>
2020-06-09 16:08:25 +0900

    update package_data

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="qt-tree",
-    version="0.1.0",
+    version="0.1.2",
     author="Kotaro Kikuchi",
     author_email="ktrk115@gmail.com",
     description="Graphical user interface for editing and visualizing tree data structure",
@@ -10,6 +10,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/ktrk115/qt-tree",
     packages=setuptools.find_packages(),
+    package_data={"": ["default_config.json"]},
     install_requires=['PySide2', 'anytree'],
     extras_require={'pillow': ['pillow']},
     keywords='Qt, tree structure, tree editor',
